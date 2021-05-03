2b373fe2d5407cd31a15e619964013ee4b0cda87
francisco ximenes <fximenessilva@gmail.com>
2021-05-03 15:31:39 +0100

    add empty users

---
+++ users-list/src/components/UsersList/EmptyUsers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EmptyUsers = () => (
+  <div>
+    <h1 style={{ margin: '50px 0px 330px' }}>There are no more users...</h1>
+  </div>
+);
+
+export default EmptyUsers;
