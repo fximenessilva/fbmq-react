be30c29298662b7938f303895eec655c4952cf7e
francisco ximenes <fximenessilva@gmail.com>
2021-05-03 14:57:23 +0100

    add users list

--- users-list/src/components/Header/Header.jsx
+++ users-list/src/components/Header/Header.jsx
@@ -8,9 +8,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import './styles/Header.css';
 
 const Header = () => {
-  const [search, setSearch] = useState('');
-  const [inputSelected, setInputSelected] = useState(false);
-
   const dispatch = useDispatch();
 
   return (
@@ -18,23 +15,7 @@ const Header = () => {
       <div className="logo-wrapper">
         <img src="https://trello-attachments.s3.amazonaws.com/60411589f3a6285816a489c0/608fda773e58683a37493fd0/a14428120d34a65adc0f6dc9034fc79b/fabamaqLogo.png" alt="logo" />
       </div>
-      <div className="input-wrapper">
-        <input
-          autoFocus
-          className={`${inputSelected ? 'inputSelected' : ''}`}
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          onBlur={() => { setInputSelected(!inputSelected); setSearch(''); }}
-        />
-        <span
-          type="button"
-          onClick={() => { setInputSelected(!inputSelected); }}
-          className={`material-icons ${inputSelected && 'searchIcon-unSelect'}`}
-        >
-          search
-        </span>
-      </div>
+
     </header>
   );
 };
--- users-list/src/components/UsersList/UsersList.jsx
+++ users-list/src/components/UsersList/UsersList.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,19 +15,61 @@ const UsersList = () => {
   const userList = useSelector((state) => state.userReducer);
 
   const { users } = userList;
+  if (users?.data) {
+    console.log(users.data);
+  }
+
+  function setMin() {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }
+
+  function setMax() {
+    if (users.data?.length === 6) {
+      setPage(page + 1);
+    }
+  }
 
   useEffect(() => {
-    dispatch(requestUsers(1));
-  }, [dispatch]);
+    dispatch(requestUsers(page));
+  }, [dispatch, page]);
   return (
 
     <div className="usersList-wrapper">
       {users?.data && users?.data.length && users?.data.map((user) => (
-        <div key={user.id} className="userCard">
-          <li key={user.id}>{user.first_name}</li>
-        </div>
-      ))}
+        <li key={user.id}>
+          <div key={user.id} className="userCard">
+            <div className="avatar-wrapper">
+              <img src={user.avatar} alt="" />
+            </div>
+            <div className="user-details">
+              <span className="user-name">
+                {user.first_name}
+                {' '}
+                {user.last_name}
+              </span>
+              <span className="user-email">
+                {user.email}
+              </span>
+            </div>
 
+          </div>
+        </li>
+      ))}
+      <div className="navigation-btn-wrapper">
+        <span role="button" id="beforePageBtn" onClick={() => setMin()} className="material-icons">
+          navigate_before
+        </span>
+        <p>
+          Page
+          {' '}
+          {page}
+        </p>
+        <span role="button" id="nextPageBtn" onClick={() => setMax()} type="button" className="material-icons">
+          navigate_next
+        </span>
+      </div>
     </div>
   );
 };
