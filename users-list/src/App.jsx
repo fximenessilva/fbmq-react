1532e88f29bd29a1c3d4926edd8c71a118d0dbd1
francisco ximenes <fximenessilva@gmail.com>
2021-05-03 15:05:52 +0100

    add footer

--- users-list/src/App.jsx
+++ users-list/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import UsersList from './components/UsersList/UsersList';
+import Footer from './components/Footer/Footer';
 
 import './styles/App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <UsersList />
+      <Footer />
     </>
   );
 }
